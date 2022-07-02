58d1cb3ac5da3f94685f4c403150a1823b16f0fd
Colin Rundel <rundel@gmail.com>
2022-07-01 20:40:43 -0400

    quarto 0.9.637

--- quarto.rb
+++ quarto.rb
@@ -1,8 +1,8 @@
 class Quarto < Formula
   desc "Scientific and technical publishing system built on Pandoc"
   homepage "https://www.quarto.org/"
-  url "https://github.com/quarto-dev/quarto-cli/releases/download/v0.9.629/quarto-0.9.629-macos.tar.gz"
-  sha256 "7974fa3936fedd7b6ed7f3353b83dbcca1dae450b5e80b9c30050dc9babb38ab"
+  url "https://github.com/quarto-dev/quarto-cli/releases/download/v0.9.637/quarto-0.9.637-macos.tar.gz"
+  sha256 "c3fa8f32539e8ef5de898c692ed2432ff07f6fa83226524b7fcd02bba4ab80d2"
   license "GPL-2.0-or-later"
 
   def install
