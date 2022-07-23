3e8244f60af765a2ff9210b34e3d6cbbd480e2af
Colin Rundel <rundel@gmail.com>
2022-07-23 15:07:26 -0400

    Split quarto to latest release and dev release

--- quarto-dev.rb
+++ quarto-dev.rb
@@ -1,14 +1,9 @@
-class Quarto < Formula
-  desc "Scientific and technical publishing system built on Pandoc"
+class QuartoDev < Formula
+  desc "Scientific and technical publishing system built on Pandoc - latest development pre-release"
   homepage "https://www.quarto.org/"
-  url "https://github.com/quarto-dev/quarto-cli/releases/download/v1.0.1/quarto-1.0.1-macos.tar.gz"
+  url "https://github.com/quarto-dev/quarto-cli/releases/download/v1.1.3/quarto-1.1.3-macos.tar.gz"
   sha256 "62179042d82a4fe7c7341ee3fe91193840c8256761fba969faa1d7d95e14ad71"
   license "GPL-2.0-or-later"
-
-  livecheck do
-    url :stable
-    strategy :github_latest
-  end
 
   def install
     prefix.install Dir["*"]
