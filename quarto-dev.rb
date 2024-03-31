4297b673389abcbe6209abbc4f2419399fd5585b
Colin Rundel <rundel@gmail.com>
2024-03-31 10:20:37 -0400

    quarto-dev 1.5.27

--- quarto-dev.rb
+++ quarto-dev.rb
@@ -1,8 +1,8 @@
 class QuartoDev < Formula
   desc "Scientific and technical publishing system built on Pandoc (pre-release)"
   homepage "https://www.quarto.org/"
-  url "https://github.com/quarto-dev/quarto-cli/releases/download/v1.5.25/quarto-1.5.25-macos.tar.gz"
-  sha256 "d2e28132875b5e77ad84be53a45289dc7e3a9de929297f26379a04b7144a0e84"
+  url "https://github.com/quarto-dev/quarto-cli/releases/download/v1.5.27/quarto-1.5.27-macos.tar.gz"
+  sha256 "39fb4c89625f8a037c72671374d8c02cc2b5eb38682bc08b4e588d0eef690bc2"
   license "GPL-2.0-or-later"
 
   livecheck do
