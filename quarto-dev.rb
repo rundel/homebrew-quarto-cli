abef976f4277fe8bd70ad489063f412c25bc7e73
alisherj <45711590+juzgenbayev@users.noreply.github.com>
2025-05-02 10:17:43 -0500

    correct sha for 181

--- quarto-dev.rb
+++ quarto-dev.rb
@@ -2,7 +2,7 @@ class QuartoDev < Formula
   desc "Scientific and technical publishing system built on Pandoc (pre-release)"
   homepage "https://www.quarto.org/"
   url "https://github.com/quarto-dev/quarto-cli/releases/download/v1.8.1/quarto-1.8.1-macos.tar.gz"
-  sha256 "252a2bb734b77f6497deb07903167a68e84cab1c5bdfdb20c3c37a5065c1a676"
+  sha256 "cadb4f2fab247a5eeaf5baee16252c4f73b783fb6ee1f55b62f355bc56f609b8"
   license "GPL-2.0-or-later"
 
   livecheck do
