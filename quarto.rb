a30f60e6609aa12505b9396f100f7827ffd1eb43
alisherj <45711590+juzgenbayev@users.noreply.github.com>
2025-05-02 11:30:59 -0500

    Update non-dev

--- quarto.rb
+++ quarto.rb
@@ -1,8 +1,8 @@
 class Quarto < Formula
   desc "Scientific and technical publishing system built on Pandoc"
   homepage "https://www.quarto.org/"
-  url "https://github.com/quarto-dev/quarto-cli/releases/download/v1.6.43/quarto-1.6.43-macos.tar.gz"
-  sha256 "1ead22fde52301cdc07d3c294a6003a05b36f1c570805cfefbff0e8dc4beb2cd"
+  url "https://github.com/quarto-dev/quarto-cli/releases/download/v1.7.29/quarto-1.7.29-macos.tar.gz"
+  sha256 "a7363a81460a328dda841aa82239a354109e93a13e6073c88b3adf6e0f71cdf1"
   license "GPL-2.0-or-later"
 
   livecheck do
