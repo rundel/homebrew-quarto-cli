8155f186ec86d9841ead747d1170bc7ffefe9deb
alisherj <45711590+juzgenbayev@users.noreply.github.com>
2025-05-02 10:15:26 -0500

    1.8.1

--- quarto-dev.rb
+++ quarto-dev.rb
@@ -1,7 +1,7 @@
 class QuartoDev < Formula
   desc "Scientific and technical publishing system built on Pandoc (pre-release)"
   homepage "https://www.quarto.org/"
-  url "https://github.com/quarto-dev/quarto-cli/releases/download/v1.7.27/quarto-1.7.27-macos.tar.gz"
+  url "https://github.com/quarto-dev/quarto-cli/releases/download/v1.8.1/quarto-1.8.1-macos.tar.gz"
   sha256 "252a2bb734b77f6497deb07903167a68e84cab1c5bdfdb20c3c37a5065c1a676"
   license "GPL-2.0-or-later"
 
